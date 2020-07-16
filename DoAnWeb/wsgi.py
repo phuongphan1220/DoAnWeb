9604e5d1db32566ad3f65fe955023aa18f34708b
Phuong Phan <phuongphan.1220@gmail.com>
2020-07-17 00:50:40 +0700

    wsgi

--- DoAnWeb/wsgi.py
+++ DoAnWeb/wsgi.py
@@ -6,11 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+path = '/home/doanwebcnpm/DoAnWeb'
+if path not in sys.path:
+ sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'DoAnWeb.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DoAnWeb.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
